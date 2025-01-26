88001ef4966342920f648991de281091a18173e8
Jason Livesay <runvnc@gmail.com>
2025-01-26 17:17:15 -0600

    remove non-interleaved message

--- src/mr_deepseek/mod.py
+++ src/mr_deepseek/mod.py
@@ -33,7 +33,16 @@ async def stream_chat(model, messages=[], context=None, num_ctx=200000,
         print("DeepSeek stream_chat (OpenAI compatible mode)")
         
         model_name = os.environ.get("AH_OVERRIDE_LLM_MODEL", "deepseek-reasoner")
-        
+
+        # look at the last message and the one before that
+        # if the role of both of them is the same
+        # this is not valid
+        # so we need to remove the last message
+        last_role = messages[-1]['role']
+        second_last_role = messages[-2]['role']
+        if last_role == second_last_role:
+            messages = messages[:-1]
+
         messages = [concat_text_lists(m) for m in messages]
 
         stream = await client.chat.completions.create(
